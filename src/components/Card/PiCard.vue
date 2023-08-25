908bf61e59cde43d8f378fdf49af4528f26d532c
Neil-Lin <neillin1023@gmail.com>
2023-08-25 16:55:29 +0800

    fix: card

--- src/components/Card/PiCard.vue
+++ src/components/Card/PiCard.vue
@@ -40,3 +40,43 @@ defineProps({
   }
 })
 </script>
+
+<style scoped>
+.pi-card {
+  border: 1px solid oklch(var(--dialog-border-color));
+  border-radius: var(--radius);
+  & .pi-card-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: var(--spacing-xs) var(--spacing-m);
+    border-bottom: 1px solid oklch(var(--dialog-border-color));
+    background-color: oklch(var(--dialog-header-bg));
+    border-radius: var(--radius) var(--radius) 0 0;
+  }
+  & .pi-card-container {
+    padding: var(--spacing-m);
+    background-color: oklch(var(--dialog-content-bg));
+    word-break: break-all;
+    word-break: break-word;
+    border-radius: var(--radius);
+  }
+  & .pi-card-footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    padding: var(--spacing-xs) var(--spacing-m);
+    border-top: 1px solid oklch(var(--dialog-border-color));
+    background-color: oklch(var(--dialog-header-bg));
+    border-radius: 0 0 var(--radius) var(--radius);
+    ~.pi-card-container {
+      border-radius: 0;
+    }
+  }
+  &.pi-card--shadow {
+    box-shadow: var(--box-shadow);
+  }
+}
+</style>
